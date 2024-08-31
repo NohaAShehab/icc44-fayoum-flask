e6503de00f0fff032cbe4b3dd3c9411e3c2b984d
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-08-31 10:49:31 +0300

    using templates , url names , templates inheritance

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import render_template
+
 
 # starting the application inside this script use, the __name__ entry point for it
 app = Flask(__name__)
@@ -47,6 +49,52 @@ def student_details(id):
 
     return "<h1>Not found </h1>"
 
+
+
+###3 #######################################################use templates
+@app.route("/landing")
+def land():
+    return  render_template("landing.html")
+
+
+# base template , then inherit --> persons --> return with response --> send it to the template
+
+@app.route("/persons")
+def persons():
+    persons = [
+        {"id": 1, "name": "Mazen", "image": "pic1.png", "grade": 100},
+        {"id": 2, "name": "Safa", "image": "pic2.png", "grade": 100},
+        {"id": 3, "name": "Israa", "image": "pic3.png", "grade": 100},
+        {"id": 4, "name": "Omar", "image": "pic4.png", "grade": 100},
+        {"id": 5, "name": "Helana", "image": "pic4.png", "grade": 100},
+    ]
+    return render_template("persons/index.html", name='noha', track="python", persons = persons)
+
+
+@app.route("/persons/<int:id>", endpoint="person.show")
+def person_profile(id):
+    persons = [
+        {"id": 1, "name": "Mazen", "image": "pic1.png", "grade": 100},
+        {"id": 2, "name": "Safa", "image": "pic2.png", "grade": 100},
+        {"id": 3, "name": "Israa", "image": "pic3.png", "grade": 100},
+        {"id": 4, "name": "Omar", "image": "pic4.png", "grade": 100},
+        {"id": 5, "name": "Helana", "image": "pic4.png", "grade": 100},
+    ]
+
+    person= list(filter(lambda p: p["id"]==id , persons))# filter object casted to list
+    # print(list(student))
+    if person:
+        return render_template("persons/show.html", person=person[0])
+
+    return "<h1>Not found </h1>"
+
+
+
+# we need to start from terminal
+# open it
+# export FLASK_APP=app
+# export DEBUG=True
+# then run flask run
 
 # start server
 if __name__ == "__main__":
